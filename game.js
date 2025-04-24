5fe24c8841f01eda0c4c6435aca725d4344411ca
abhinandan7781 <abhinandan.jain.2108@gmail.com>
2025-04-24 12:01:31 +0530

    ScoreCard UPdated

--- game.js
+++ game.js
@@ -1,11 +1,10 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
-
 var gamePattern = [];
 var userClickedPattern = [];
 
 var started = false;
 var level = 0;
+var highestLevel = 0;
 
 $(document).keypress(function() {
   if (!started) {
@@ -16,42 +15,45 @@ $(document).keypress(function() {
 });
 
 $(".btn").click(function() {
-
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
-
-  checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function checkAnswer(currentLevel) {
-
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-    } else {
-      playSound("wrong");
-      $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
-
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
       setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-
-      startOver();
+        nextSequence();
+      }, 1000);
     }
-}
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
 
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    startOver();
+  }
+}
 
 function nextSequence() {
   userClickedPattern = [];
   level++;
+  if (level > highestLevel) {
+    highestLevel = level;
+    $("#highest-level").text(highestLevel);
+  }
+
   $("#level-title").text("Level " + level);
+  $("#current-level").text(level);
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -76,4 +78,5 @@ function startOver() {
   level = 0;
   gamePattern = [];
   started = false;
+  $("#current-level").text("0");
 }
